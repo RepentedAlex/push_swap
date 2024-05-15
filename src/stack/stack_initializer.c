9fa428cd2abae71ec3f97a3345e6997f8d534154
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-05-15 13:14:13 +0200

    Added function to initialize stack

---
+++ src/stack/stack_initializer.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_initializer.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 12:52:30 by apetitco          #+#    #+#             */
+/*   Updated: 2024/05/15 12:55:02 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_printf.h"
+#include "push_swap.h"
+
+int	init_stack(t_stack **stack_a, char **split)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	stack_a = NULL;
+	if (!split)
+		return (-1);
+	while (split[i])
+		i++;
+	while (--i >= 0)
+	{
+		j = add_to_list(stack_a, ft_atoi(split[i]));
+		if (j == -1)
+			return (-1);
+		free(split[i]);
+	}
+	free(split);
+	return (1);
+}
