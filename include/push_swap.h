9dc6e9a166f7792aec1d4326845d15228a9468ff
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-05-15 10:41:02 +0200

    Added utilitaries for stack manipulation

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:08:27 by apetitco          #+#    #+#             */
-/*   Updated: 2024/05/15 10:13:38 by apetitco         ###   ########.fr       */
+/*   Updated: 2024/05/15 10:40:33 by apetitco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,11 @@ typedef struct s_mq
 	int	q1;
 	int	q3;
 }		t_mq;
+
+int		add_to_list(t_stack **stack, int num);
+void	free_everything(t_stack **stack_a, t_stack **stack_b, t_mq **mq);
+void	free_stack(t_stack **stack);
+int		get_stack_len(t_stack *stack);
+t_stack	*new_node(int num);
 
 #endif
