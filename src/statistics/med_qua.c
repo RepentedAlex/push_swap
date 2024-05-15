63df55550170d84a105a056c147768570d4f3339
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-05-15 12:32:37 +0200

    Wrote the `quick_sort` function

--- src/statistics/med_qua.c
+++ src/statistics/med_qua.c
@@ -6,13 +6,56 @@
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:43:07 by apetitco          #+#    #+#             */
-/*   Updated: 2024/05/15 11:25:11 by apetitco         ###   ########.fr       */
+/*   Updated: 2024/05/15 12:32:04 by apetitco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_printf.h"
 #include "push_swap.h"
+
+static void	swap_el(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+static int partition (int array[], int low, int high)
+{
+	int	pivot;
+	int	i;
+	int	j;
+
+	pivot = array[high];
+	i = low - 1;
+	j = low;
+	while (array[j] <= pivot)
+	{
+		if (arrau[j] <= pivot)
+		{
+			i++;
+			swap_el(&array[i], &array[j]);
+		}
+		j++;
+	}
+	swap_el(&array[i + 1], &array[high]);
+	return (i + 1);
+}
+
+static void quick_sort(int array[], int low, int high)
+{
+	int	pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, low, high);
+		quick_sort(array, low, pivot - 1);
+		quick_sort(array, pivot + 1, high);
+	}	
+}
 
 void	get_mq(char **split, t_mq **mq, int *error)
 {
@@ -36,7 +79,7 @@ void	get_mq(char **split, t_mq **mq, int *error)
 		free(split[i]);
 	}
 	free(split);
-	//TODO: quick_sort
+	quick_sort(array, 0, len - 1);
 	//TODO: doublon_check
 	(*mq)->med = array[((*mq)->med)];
 	free(array);
