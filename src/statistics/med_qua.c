6f3de4be41a90f703493a5ff6a4a71325d365384
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-05-15 11:10:49 +0200

    Started to make function to get median and quartiles of the serie

---
+++ src/statistics/med_qua.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   med_qua.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 10:43:07 by apetitco          #+#    #+#             */
+/*   Updated: 2024/05/15 11:08:28 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_printf.h"
+#include "push_swap.h"
+
+void	get_mq(char **split, t_mq **mq, int *error)
+{
+	int	*array;
+	int	i;
+	int	len;
+
+	len = 0;
+	while (split[len] && split != NULL)
+		++len;
+	array = malloc(sizeof(int) * len);
+	if (!array || !split)
+	{
+		*mq = NULL;
+		return ;
+	}
+	i = len;
+	while (--i != -1)
+	{
+		array[i] = atoi_check(split[i], error);
+		free(split[i]);
+	}
+	free(split);
+	//quick_sort
+	//doublon_check
+	(*mq)->med = array[((*mq)->med)];
+	free(array);
+}
