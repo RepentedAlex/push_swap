96dc3d8e97016538d3bdc7f4065c5d94bca572a1
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-05-15 10:14:15 +0200

    Added structures and enum definitions for project

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,13 +6,41 @@
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:08:27 by apetitco          #+#    #+#             */
-/*   Updated: 2024/05/15 10:08:29 by apetitco         ###   ########.fr       */
+/*   Updated: 2024/05/15 10:13:38 by apetitco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+typedef enum e_op
+{
+	sa,
+	sb,
+	ss,
+	pa,
+	pb,
+	ra,
+	rb,
+	rr,
+	rra,
+	rrb,
+	rrr
+}	t_op;
 
+typedef struct s_stack
+{
+	int				value;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}					t_stack;
+
+typedef struct s_mq
+{
+	int	len;
+	int	med;
+	int	q1;
+	int	q3;
+}		t_mq;
 
 #endif
