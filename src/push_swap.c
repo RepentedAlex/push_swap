eaa9b0620edac23d9078e77d689b64add94a3055
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-05-15 10:10:44 +0200

    Added includes in push_swap.c file

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,8 +6,10 @@
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:08:51 by apetitco          #+#    #+#             */
-/*   Updated: 2024/05/15 10:08:56 by apetitco         ###   ########.fr       */
+/*   Updated: 2024/05/15 10:10:18 by apetitco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+#include "libft.h"
 #include "push_swap.h"
