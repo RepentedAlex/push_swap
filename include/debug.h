7ddff64a1964aa97b104e7cdb378911376d3e54f
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-05-15 10:24:47 +0200

    Added `debug_stacks` function

--- include/debug.h
+++ include/debug.h
@@ -6,13 +6,13 @@
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:15:42 by apetitco          #+#    #+#             */
-/*   Updated: 2024/05/15 10:15:44 by apetitco         ###   ########.fr       */
+/*   Updated: 2024/05/15 10:23:43 by apetitco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEBUG_H
 # define DEBUG_H
 
-
+void	debug_stacks(t_stack *a, t_stack *b)
 
 #endif
---
+++ src/debug.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   debug.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/15 10:16:40 by apetitco          #+#    #+#             */
+/*   Updated: 2024/05/15 10:23:23 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "debug.h"
+#include "ft_printf.h"
+#include "libft.h"
+#include "push_swap.h"
+
+static void	dump_node(int val)
+{
+	ft_printf("val %d\n", val);
+}
+
+static void	ft_lstiter(t_stack *lst, void (*f)(int))
+{
+	while (lst)
+	{
+		f(lst->value);
+		lst = lst->next;
+	}
+}
+
+void	debug_stacks(t_stack *a, t_stack *b)
+{
+	ft_printf("Stack a:\n");
+	ft_lstiter(a, dump_node);
+	ft_printf("Stack b:\n");
+	ft_lstiter(b, dump_node);
+}
