57889cce36b92951cb3f5576d06c28e2d0ab204d
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-05-16 15:19:11 +0200

    Files should be OK

--- src/statistics/check_args.c
+++ src/statistics/check_args.c
@@ -6,7 +6,7 @@
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:58:18 by apetitco          #+#    #+#             */
-/*   Updated: 2024/05/15 12:35:14 by apetitco         ###   ########.fr       */
+/*   Updated: 2024/05/15 14:14:56 by apetitco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,10 @@
 #include "ft_printf.h"
 #include "push_swap.h"
 
-char	**args_support(int argc, char *argv[])
-{
-	int		i;
-	char	**ret;
-
-	if (argc == 2)
-		return (ft_split(argv[1], ' '));
-	return (NULL);
-}
-
 int	atoi_check(const char *ptr, int *error)
 {
 	char	*tmp;
 
-	if (ft_strlen(ptr) > 11)
-	{
-		*error = 1;
-		return (1);
-	}
 	tmp = ft_itoa(ft_atoi(ptr));
 	if (ft_strcmp(tmp, ptr))
 	{
--- src/statistics/mq_calc.c
+++ src/statistics/mq_calc.c
@@ -6,7 +6,7 @@
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 12:39:16 by apetitco          #+#    #+#             */
-/*   Updated: 2024/05/15 12:43:19 by apetitco         ###   ########.fr       */
+/*   Updated: 2024/05/16 14:12:24 by apetitco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,16 @@ void	fill_mq(t_mq **mq, int len, int array[])
 		(*mq)->med = len / 2;
 	(*mq)->q1 = (((*mq)->med + 1) / 2);
 	if ((*mq)->med % 2 == 0)
-		(*mq)->q1 = ((*mq).med / 2);
-	(*mq).q3 = ((*mq)->med + (*mq)->q1);
+		(*mq)->q1 = ((*mq)->med / 2);
+	(*mq)->q3 = ((*mq)->med + (*mq)->q1);
 	if (len > 5)
 	{
-		(*mq).q1 = array[(*mq).q1];
-		(*mq).q3 = array[(*mq).q3];
+		(*mq)->q1 = array[(*mq)->q1];
+		(*mq)->q3 = array[(*mq)->q3];
 	}
 	else
 	{
-		(*mq).q1 = 0;
-		(*mq).q3 = 0;
+		(*mq)->q1 = 0;
+		(*mq)->q3 = 0;
 	}
 }
