11a60aae1775d162161269768c9e49ebacc61a05
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-05-16 18:23:06 +0200

    Function for several argc support

--- src/statistics/check_args.c
+++ src/statistics/check_args.c
@@ -6,7 +6,7 @@
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:58:18 by apetitco          #+#    #+#             */
-/*   Updated: 2024/05/15 14:14:56 by apetitco         ###   ########.fr       */
+/*   Updated: 2024/05/16 18:07:21 by apetitco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,25 @@ int	atoi_check(const char *ptr, int *error)
 	}
 	free(tmp);
 	return (ft_atoi(ptr));
+}
+
+char	**args_support(int argc, char *argv[])
+{
+	int		i;
+	char	**split;
+	
+	if (argc == 2)
+		return (ft_split(argv[1], ' '));
+	else
+	{
+		i = 0;
+		split = malloc(sizeof(char *) * argc);
+		while (++i < argc)
+		{
+			split[i - 1] = ft_strdup(argv[i]);
+			
+		}
+		split[argc - 1] = NULL;
+		return (split);
+	}
 }
