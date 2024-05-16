8510ac5f69416dbee29577f92f2acaaa6cd580ae
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-05-16 14:51:53 +0200

    Pinpointed crash reason in push_swap.c file

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:08:51 by apetitco          #+#    #+#             */
-/*   Updated: 2024/05/15 13:12:23 by apetitco         ###   ########.fr       */
+/*   Updated: 2024/05/16 14:49:39 by apetitco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,22 @@ int	main(int argc, char *argv[])
 	if (!mq)
 		exit(1);
 	error = 0;
-	get_mq(args_support(argc, argv), &mq, &error);
+	get_mq(ft_split(argv[1], ' '), &mq, &error);
 	if (error || mq->len == 0 || !mq)
 	{
 		ft_putstr_fd("Error\n", 2);
 		if (mq)
 			free(mq);
 		return (1);
-	}
-	if (init_stack(&stack_a, args_support(argc, argv)) == 1)
+	}	
+	//TODO: Fix `init_stack` function
+	if (init_stack(&stack_a, ft_split(argv[1], ' ')) == 1)
+	{
+		//! PRINTF ICI !//
+		ft_printf("\033[0;93mInitialisation de la stack effectuée ! o7\n\033[0;39m");
+		//! PRINTF ICI !//
 		launch_algo(stack_a, stack_b, mq);
+	}
 	free_stack(&stack_a);
 	free_stack(&stack_b);
 	free(mq);
