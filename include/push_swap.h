d16acc786e2bc534cb350c5e4adf78d423be9e3a
RepentedAlex <petitcollin.alessandro@gmail.com>
2024-05-17 12:01:14 +0200

    Added optimize_a prototype to header file

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:08:27 by apetitco          #+#    #+#             */
-/*   Updated: 2024/05/17 11:54:38 by apetitco         ###   ########.fr       */
+/*   Updated: 2024/05/17 12:00:21 by apetitco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,8 @@ int		get_stack_len(t_stack *stack);
 int		init_stack(t_stack **stack_a, char **split, t_stack **stack_b);
 void	launch_algo(t_stack *stack_a, t_stack *stack_b, t_mq *mq);
 t_stack	*new_node(int num);
+void	optimize_a(t_stack *stack_a, t_stack *stack_b, t_mq *mq);
+void	optimize_b(t_stack *stack_a, t_stack *stack_b, t_mq *mq);
 void	push_stack(t_stack **stack_a, t_stack **stack_b, t_op op);
 void	reverse_rotate(t_stack **stack_a, t_stack **stack_b, t_op op);
 void	rotate(t_stack **stack_a, t_stack **stack_b, t_op op);
---
+++ src/algo/algo_second.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algo_second.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 11:57:41 by apetitco          #+#    #+#             */
+/*   Updated: 2024/05/17 12:00:28 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_printf.h"
+#include "push_swap.h"
+
+void	optimize_a(t_stack *stack_a, t_stack *stack_b, t_mq *mq)
+{
+	
+}
